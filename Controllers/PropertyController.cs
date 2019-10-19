f6803e25ae8e7744a7abb0e1a1dcbfe98df416c8
unknown <ajd.ocon@gmail.com>
2019-10-19 10:20:48 -0500

    added Add A house layout

--- Controllers/PropertyController.cs
+++ Controllers/PropertyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using property_master.Models;
 
@@ -13,6 +14,51 @@ namespace property_master.Controllers
         public IActionResult Property_list()
         {
             return View();
+        }
+        
+        public IActionResult add_listing()
+        {
+            return View();
+        }
+        public IActionResult add_description(string property_name,int category)
+        {
+        //   var con = this.CreateConnection();
+        //   string cText = $"select * from products where id = {id}";
+        //   MySqlCommand cm = new MySqlCommand(cText, con);
+        //   var result = cm.ExecuteReader();
+        //   while(result.Read())
+        //   {
+        //     Product p = new Product();
+        //     p.id = Convert.ToInt32(result["id"]);
+        //     p.name = result["name"].ToString();
+        //     p.price = Convert.ToDouble(result["price"]);
+        //     p.pictureUrl = result["pictureUrl"].ToString();
+        //     ViewBag.Product = p;
+        //   }
+                ViewData["ad_name"]=property_name;
+                ViewData["category"]=category;
+
+          return View();
+        }
+        [HttpPost]
+        public IActionResult add_description(IFormFile outside)
+        {
+        //   var con = this.CreateConnection();
+        //   string cText = $"select * from products where id = {id}";
+        //   MySqlCommand cm = new MySqlCommand(cText, con);
+        //   var result = cm.ExecuteReader();
+        //   while(result.Read())
+        //   {
+        //     Product p = new Product();
+        //     p.id = Convert.ToInt32(result["id"]);
+        //     p.name = result["name"].ToString();
+        //     p.price = Convert.ToDouble(result["price"]);
+        //     p.pictureUrl = result["pictureUrl"].ToString();
+        //     ViewBag.Product = p;
+        //   }
+ 
+
+          return View();
         }
 
 
